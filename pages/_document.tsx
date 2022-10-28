3ce09acda810c99bf7eb4573a426ac60cce986bc
Nicolas Tudela <nicolastudela@gmail.com>
2022-10-28 17:41:36 +0200

    [ISSUE-81] moving to local hosted fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,11 +16,6 @@ class MyDocument extends Document {
           <meta property="og:locale" content="es_AR"/>
           <meta property="og:type" content="website"/>
           <meta name="author" content="Nicolas Tudela"></meta>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Aguafina+Script&family=MonteCarlo&display=swap" rel="stylesheet"></link>
-          <link href="https://fonts.googleapis.com/css2?family=MonteCarlo&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
