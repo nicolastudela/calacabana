0e86b1d9ab11e2e72af7aa7441e1ccd311bdf88e
Nicolas Tudela <nicolastudela@gmail.com>
2023-01-12 14:51:20 +0100

    forgot the layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,22 +9,21 @@ import {
 // import MobileHeader from "@/components/Header/MobileHeader";
 // import Footer from "@/components/Footer";
 
-// const Footer = dynamic(() => import("@/components/Footer"));
-// const MobileHeader = dynamic(() => import("@/components/Header/MobileHeader"));
-// const DesktopHeader = dynamic(() => import("@/components/Header/DesktopHeader"));
+const Footer = dynamic(() => import("@/components/Footer"));
+const MobileHeader = dynamic(() => import("@/components/Header/MobileHeader"));
+const DesktopHeader = dynamic(() => import("@/components/Header/DesktopHeader"));
 
 const Layout = ({ children, ...rest }: StackProps) => {
   return (
       <Container display="flex" flexDirection={"column"} maxWidth={"container.xl"} px="0" {...rest}>
-        {/* <MobileHeader display={{base: "block", md: "none"}}/> */}
-        {/* <DesktopHeader display={{base: "none", md: "flex"}}  /> */}
+        <MobileHeader display={{base: "block", md: "none"}}/>
+        <DesktopHeader display={{base: "none", md: "flex"}}  />
         <chakra.main as="main" width={"100%"} mt={"0 !important"}>
             {children}
         </chakra.main>
-        {/* <Footer /> */}
+        <Footer />
       </Container>
   );
-  // return <>{children}</>
 };
 
 export default Layout;
